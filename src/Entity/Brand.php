5848b1d3333f46e621d0ba8305a39c7e7db018da
Tan <hoductan@gmail.com>
2022-06-29 13:54:17 +0700

    fix _nav

---
+++ src/Entity/Brand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BrandRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BrandRepository::class)
+ */
+class Brand
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Origin;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getOrigin(): ?string
+    {
+        return $this->Origin;
+    }
+
+    public function setOrigin(string $Origin): self
+    {
+        $this->Origin = $Origin;
+
+        return $this;
+    }
+}
